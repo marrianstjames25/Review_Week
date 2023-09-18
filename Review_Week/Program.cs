c5c8117e91926840aef4220a8506299a082f06b1
m.yovchevski <m.yovchevski@LAB108PC23>
2023-09-18 12:17:19 +0300

    Add project files.

---
+++ Review_Week/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student = new Student();
+            student.age = 14;
+            student.age = 98;
+            student.Name = "Test";
+
+        }
+    }
+    class Student
+    {
+        private string _name; //Auto Implemented Property
+        public int age;
+        //public int phoneNum { get; set; }
+        public double GPA;
+
+        //Explicitly
+        public string Name
+        {
+            get
+            {
+                if (age > 18)
+                {
+                    return _name;
+                }
+                else
+                {
+                    return "Student is underage";
+                }
+            }
+            set
+            {
+                if (age  != 0) 
+                {
+                _name = value;}
+            }
+        }
+
+        public Student(string name, int age, double GPA)
+        {
+            this.age = 16;
+            this.GPA = GPA;
+            Name = name;
+
+        }
+
+
+    }
+
+}
---
+++ Review_Week/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_Week
+{
+    internal class Student
+    {
+        //props
+        //constructor
+        //methods
+
+    }
+}
